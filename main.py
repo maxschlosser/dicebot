03620c21d9c61602d2680cdb8e751e92cc20c4fe
Maximilian Schlosser <maxschlosser96@googlemail.com>
2021-01-12 17:40:01 +0100

    fuck person

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def roll(ctx, dice: int, sides: int):
 
 @bot.command(name='fuck', help='fuck', pass_context=True)
 async def fuck(ctx):
-    await ctx.send("Fuck " + ctx.message.author.mention)
+    await ctx.send("Fuck you, " + ctx.message.author.mention + "!")
 
 
 @roll.error
